7947edce1fca5b9f4bbc798d3d4cdf2f5db488c3
Adam Linscott <adam.linscott@probablyrational.com>
2017-11-09 09:51:36 +0000

    background

--- program.py
+++ program.py
@@ -12,11 +12,15 @@ screen = pygame.display.set_mode((width, height))
 
 #load player
 player = pygame.image.load("resources/images/dude.png")
+grass = pygame.image.load("resources/images/grass.png")
 
 
 #Game loop
 while True:
 	#draw image on screen
+	for x in range(width/grass.get_width()+1):
+		for y in range(height/grass.get_height()+1):
+			screen.blit(grass,(x*100,y*100))
 	screen.blit(player, (100,100))
 	#update window
 	pygame.display.flip()
